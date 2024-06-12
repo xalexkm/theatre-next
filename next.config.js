2054f7a5dbc2db32c9b0d863ab8490ef8bda7401
xalexkm <xalexkm@gmail.com>
2024-06-12 19:12:58 +0100

    fix: attach styles to head during ssr

--- next.config.js
+++ next.config.js
@@ -9,4 +9,7 @@ module.exports = {
             },
         ],
     },
+    compiler: {
+        styledComponents: true,
+    },
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,20 @@
+import "../styles/global.css";
+import type { Metadata } from 'next'
+import StyledComponentsRegistry from "../lib/registry";
+
+export const metadata: Metadata = {
+    title: 'Theatre App',
+    description: 'Some description should be here',
+}
+
 export default function RootLayout({children}: {
     children: React.ReactNode
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+            </body>
         </html>
     )
 }
