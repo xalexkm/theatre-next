0cb89bdd36074c3fb14f0f22aa9b327bbd9c13ed
xalexkm <xalexkm@gmail.com>
2024-06-18 00:38:03 +0100

    feat: add wrapper to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import MoviesList from "../components/moviesList";
 import SearchBar from "../components/searchBar";
 import { searchMoviesByTitle } from "../lib/api";
+import Wrapper from "../components/wrapper";
 
 export default async function Page() {
   const moviesData = await searchMoviesByTitle("Fragrance");
 
   return (
     <>
-      <SearchBar></SearchBar>
-      <MoviesList moviesData={moviesData} />
+        <Wrapper>
+            <SearchBar></SearchBar>
+            <MoviesList moviesData={moviesData} />
+        </Wrapper>
     </>
   );
 }
---
+++ src/components/wrapper.tsx
@@ -0,0 +1,9 @@
+"use client"
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 1000px;
+  margin: 0 auto;
+`;
+
+export default Wrapper;
